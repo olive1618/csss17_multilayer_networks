cc267b8807c316dac7aa0060df82bc6c85188478
eshouppe <eshouppe@gmail.com>
2017-06-30 16:38:54 -0400

    Create single layer adjacency matrix

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
+import re
 import string
+import itertools
 
 import pandas as pd
 import numpy as np
@@ -135,11 +137,54 @@ def collapse_to_single_layer():
 
     base_df.to_csv('data/all_sites/AllSites.csv')
 
+#
+def create_single_layer_adjacency():
+    """Create multilayer adjacency matrices for input to MultiTensor software.
+    Pollinators are rows and plants are columns. Directed so node1 --> node2"""
+    all_sites_df = pd.read_csv(ALL_SITES_FILE_LOC, header=0, index_col=0)
+    all_plants = list(all_sites_df.columns)
+    all_pollinators = list(all_sites_df.index)
+
+    for file_name in os.listdir(SITE_DIR_LOC):
+        site_df = pd.read_csv(os.path.join(SITE_DIR_LOC, file_name), header=0, index_col=0)
+        site_num = re.findall(r'\d+', file_name)[0]
+        print "Site Number: ", site_num
+
+        # Create list of all possible pollinator-plant pairs
+        all_pairs = list(itertools.product(all_pollinators, all_plants))
+
+        outfile_name = "Site" + site_num + "_Adjacency.dat"
+        with open(os.path.join('data', 'adjacency', outfile_name), 'w') as outfile:
+            for pair in all_pairs:
+                try:
+                    pol_pretty_name = POLLINATOR_LOOKUP[pair[0]]
+                except KeyError:
+                    print("Pollinator not found: ", pair[0])
+                    pol_pretty_name = pair[0]
+
+                try:
+                    plt_pretty_name = PLANT_LOOKUP[pair[1]]
+                except KeyError:
+                    print("Plant not found: ", pair[1])
+                    plt_pretty_name = pair[1]
+
+                # Row format: E node1name node2name L1wght L2wght L3wght ...
+                row = ['E', pol_pretty_name, plt_pretty_name] + ['0'] * 14
+                # Add pol/plt count at this layer if exists, else retain 0
+                try:
+                    row[2+int(site_num)] = str(site_df.loc[pair[0], pair[1]])
+                except KeyError:
+                    pass
+
+                outfile.write(" ".join(row)+'\n')
 
 #
 if __name__ == "__main__":
     SITE_DIR_LOC = "data/sites"
-    # pollinator_lookup, plant_lookup = create_node_names()
+    ALL_SITES_FILE_LOC = "data/all_sites/AllSites.csv"
+
+    POLLINATOR_LOOKUP, PLANT_LOOKUP = create_node_names()
     # collapse_to_islands()
     # collaps_to_main_islands()
-    collapse_to_single_layer()
+    # collapse_to_single_layer()
+    create_single_layer_adjacency()
