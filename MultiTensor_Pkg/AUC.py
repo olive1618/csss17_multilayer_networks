090c21cb7390ae2d16d72fac11330067ffaf44e5
eshouppe <eshouppe@gmail.com>
2017-07-04 16:46:12 -0400

    k selection working

--- MultiTensor_Pkg/AUC.py
+++ MultiTensor_Pkg/AUC.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def calculate_AUC(M,Pos,Neg):
     # M= # SORTED (from small to big) List of 2-tuple, each entry is M[n]=(mu_ij,A_ij)      
     # Pos= # positive entries (graph edges)         
@@ -14,24 +17,28 @@ def calculate_AUC(M,Pos,Neg):
 
 def calculate_AUC_weighted(M,max_w):
     """"
-    AUC for wieghted entries A_ij. Ideally, if an observed edge has A_ij=4, it will have a bigger M entry than an observed edge with A_ij<4
+    AUC for weighted entries A_ij. Ideally, if an observed edge has A_ij=4, it will have a bigger M entry than an observed edge with A_ij<4
     - M= # SORTED (from small to big) List of 2-tuple, each entry is M[n]=(mu_ij,A_ij)    
     - m_k= multiplicity of A_ij=k entries      
     - max_w is the maximum edge weigth, needed to order the y[k]
     """
 
-    pos=0;neg=0
-    y=np.zeros(max_w+1);bad=0.; 
-    for m,a in M:
-        y[a]+=1;   # number of entries with A_ij=a already encountered
-        for q in range(a+1,max_w+1):bad+=y[q]   # number of entries with A_ij>a already encountered  --> penalty
-    # calculate denominator --> normalization    
-    Z=0;
-    for k in range(max_w+1):
-        m=0.
-        for q in range(k+1,max_w+1): m+=y[q]
-        Z+=m*y[k]
+    bad=0.
+    y=np.zeros(max_w+1)
 
-    AUC=1.-(bad/float(Z));     
+    for m, a in M:
+        y[a] += 1   # number of entries with A_ij=a already encountered
+        for q in range(a+1,max_w+1):
+            # number of entries with A_ij>a already encountered --> penalty
+            bad += y[q]
+    # calculate denominator --> normalization    
+    Z = 0
+    for k in range(max_w+1):
+        m = 0.
+        for q in range(k+1, max_w+1):
+            m += y[q]
+        Z += m*y[k]
+
+    AUC = 1.-(bad/float(Z))
 
     return AUC;
--- cmnty_detect.py
+++ cmnty_detect.py
@@ -15,10 +15,41 @@ import pandas as pd
 import numpy as np
 
 
-def calculate_directed_AUC(exp_act_ew_dict):
-    """Calculate area under the ROC curve. Ideal is for ordering of expected edge weights
-    to match to ordering of actual edge weights."""
-    pass
+def calculate_directed_auc(exp_act_ew_dict):
+    """Calculate area under the ROC curve. Ideal is for the ordering of expected
+    edge weights to match to ordering of actual edge weights.
+    sorted_m is a list of tuples (expected_ij, actual_ij) order ascending by
+    actuals"""
+    print "\nCalculate AUC for layer "
+    sum_layers_auc = 0.0
+
+    for layer in range(1, 15):
+        print layer, "..",
+        sorted_m = sorted(exp_act_ew_dict[layer], key=lambda tup: tup[0])
+        max_actual_integer = max(exp_act_ew_dict[layer], key=lambda tup: tup[1])[1]
+        print "Max actual integer:", max_actual_integer
+        observed_actuals = np.zeros(max_actual_integer+1)
+        penalty = 0.
+
+        for _, act in sorted_m:
+            # Increment position of value already encountered
+            observed_actuals[int(round(act))] += 1
+            for q in range(int(round(act))+1, max_actual_integer+1):
+                # Increment the penality by the number of entries with
+                #  A_ij>act already encoutered
+                penalty += observed_actuals[q]
+        
+        # Calculate denominator to normalize
+        denom = 0.
+        for k in range(max_actual_integer+1):
+            m = 0.
+            for p in range(k+1, max_actual_integer+1):
+                m += observed_actuals[p]
+            denom += m * observed_actuals[k]
+        
+        sum_layers_auc += 1. - (penalty / denom)
+    
+    return sum_layers_auc
 
 #
 def select_k():
@@ -28,7 +59,7 @@ def select_k():
     Steps:
     - Input AllSites_adjacency.dat to MultiTensor main.py
     - Confirm dimensions of 3 outputted matrices u, v, w
-    - Get product of u * w * v.  Output matrix should be size N * N * L
+    - Get product of u * w * v. Output matrix should be size N * N * L
     - Compare E(edge weight) with actual with AUC calculation
     - Put those steps in a loop for multiple values of k
     - Select k with highest prediction accuracy
@@ -40,6 +71,7 @@ def select_k():
         if file_name.split('_')[0] in ['u', 'v', 'w']:
             this_k = re.findall(r'\d+', file_name)[0]
             if this_k not in checked_ks:
+                print "K =", this_k
                 checked_ks.append(this_k)
                 # Read u matrix file to a dataframe and then convert to np array
                 u_df = pd.read_table(os.path.join(ALL_ADJ_DIR, 'u_K'+this_k+'.dat'),
@@ -74,7 +106,7 @@ def select_k():
                 
                 print "U shape:", np.shape(u_mat), "V shape:", np.shape(v_mat), "W shape:", np.shape(w_mat)
 
-                # Matrix multiple u and each layer of w and v. Product shape is N x N x L
+                # Matrix multiply u and each layer of w and then multiply by v. Product shape is N x N x L
                 expect_edg_wght = np.zeros((np.shape(u_mat)[0], np.shape(u_mat)[0], 14))
                 for lyr in range(14):
                     u_dot_w = np.mat(u_mat) * np.mat(w_mat[:, :, lyr])
@@ -83,25 +115,39 @@ def select_k():
                 # Ordered node names to use in comparison
                 node_names = list(v_df.index)
 
-                col_names = ["Pollinator", "Plant"] + ["L"+l for l in range(1, 15)]
+                # Read in actual edge weights from holdout file
+                col_names = ["Pollinator", "Plant"] + ["L"+str(l) for l in range(1, 15)]
                 actual_edg_wght_df = pd.read_table(os.path.join(ALL_ADJ_DIR, 'AllSites_holdout.dat'),
                                                    delim_whitespace=True,
                                                    header=None, index_col=False,
-                                                   names=col_names, usecols=[1:])
+                                                   names=col_names, usecols=[i for i in range(1, 17)])
 
-                
-                # Build list of tuples of (expected_ew, actual_ew) for AUC calculation
+                # Build dictionary of lists of tuples of (expected_ew, actual_ew) for AUC calculation
                 exp_act = {lyr: [] for lyr in range(1, 15)}
 
                 for row in actual_edg_wght_df.itertuples():
-                    pol_idx = node_names.index(row[1])
-                    plt_idx = node_names.index(row[2])
+                    try:
+                        pol_idx = node_names.index(row[1])
+                    except ValueError:
+                        pol_idx = 'x'
+                    
+                    try:
+                        plt_idx = node_names.index(row[2])
+                    except ValueError:
+                        plt_idx = 'x'
+                    
                     for idx, act_edg_wght in enumerate(row[3:]):
-                        expected_ew = expect_edg_wght[pol_idx, plt_idx, idx]
+                        if pol_idx != 'x' and plt_idx != 'x':
+                            expected_ew = expect_edg_wght[pol_idx, plt_idx, idx]
+                        else:
+                            # If either the plant or the pollinator was not in the training set, so
+                            #  no estimate is available, use expected edge weight of zero
+                            expected_ew = 0.0
                         # Add tuple to correct layer
                         exp_act[idx+1].append((expected_ew, act_edg_wght))
                 
-                calc_auc = calculate_directed_AUC(exp_act)
+                summed_auc = calculate_directed_auc(exp_act)
+                print summed_auc
 
 
 
